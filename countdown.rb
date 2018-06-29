58b941c5c30215fd7a94de54f396235d12031077
Kyle Neale <kyle.a.neale@gmail.com>
2018-06-29 20:00:42 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,18 +1,20 @@
 #write your code here
 
 def countdown(n)
-  while n > 0
-    puts "#{n} SECOND(S)!"
-    n -= 1
+  counter = n
+  while counter > 0
+    puts "#{counter} SECOND(S)!"
+    counter -= 1
   end
   "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep(n)
-    while n > 0
-    sleep 1 
-    puts "#{n} SECOND(S)!"
-    n -= 1
+  counter = n
+  while counter > 0
+    sleep 1
+    puts "#{counter} SECOND(S)!"
+    counter -= 1
   end
   "HAPPY NEW YEAR!"
 end
