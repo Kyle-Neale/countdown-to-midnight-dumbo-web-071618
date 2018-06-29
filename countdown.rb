c058d6372dd1259ad2ce4e8ce4b21dca2e511603
Kyle Neale <kyle.a.neale@gmail.com>
2018-06-29 19:59:23 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -2,6 +2,14 @@
 
 def countdown(n)
   while n > 0
+    puts "#{counter} SECOND(S)!"
+    n -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(n)
+    while n > 0
     sleep 1 
     puts "#{counter} SECOND(S)!"
     n -= 1
